b7ce3b1750ac1a59e62646e1a0058b168e242333
ytskmt14 <sorigeeeen@gmail.com>
2023-03-18 00:57:54 +0900

    add client, request

--- lib/connpass_event.rb
+++ lib/connpass_event.rb
@@ -1,8 +1,52 @@
 # frozen_string_literal: true
 
+require 'faraday'
 require_relative 'connpass_event/version'
 
+# rubocop:disable Style/AsciiComments
+
+# connpass API - イベントサーチAPI用モジュール
+# https://connpass.com/about/api/
 module ConnpassEvent
-  class Error < StandardError; end
-  # Your code goes here...
+  class ConnpassEventApiError < StandardError; end
+
+  ENDPOINT = 'https://connpass.com/api/v1'
+
+  # API Client
+  class Client
+    def initialize
+      @connection = Faraday.new(url: ENDPOINT)
+    end
+
+    # イベントサーチAPI
+    # GET /event/
+    # @params params [Hash] リクエストパラメータ
+    # @return [Hash] レスポンスボディ
+    def events(params = {})
+      response = @connection.get('event/') do |request|
+        request.params = params
+      end
+
+      raise ConnpassEventApiError, "Error is occurred. HTTP STATUS: #{response.status}" unless response.status == 200
+
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+
+  # connpass API - クエリパラメータ用クラス
+  class Request
+    PARAMETERS = %i[event_id keyword keyword_or ym ymd nickname owner_nickname series_id start order count
+                    format].freeze
+
+    def self.params(**options)
+      options.each_with_object({}) do |(key, value), result|
+        unless PARAMETERS.include?(key)
+          puts "except key: #{key}"
+          next
+        end
+        result[key] = value
+      end
+    end
+  end
 end
+# rubocop:enable Style/AsciiComments
--- spec/connpass_event_spec.rb
+++ spec/connpass_event_spec.rb
@@ -1,11 +1,55 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe ConnpassEvent do
   it 'has a version number' do
     expect(ConnpassEvent::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '::Client' do
+    describe '#initialize' do
+      it 'has instance variable @connection' do
+        client = ConnpassEvent::Client.new
+        expect(client.instance_variable_defined?(:@connection)).to eq(true)
+        expect(client.instance_variable_get(:@connection).class).to eq(Faraday::Connection)
+      end
+    end
+
+    describe '#events' do
+      before do
+        connection_mock = double('connection_mock')
+        response_mock = double('response_mock', status: 200, body: File.read('spec/fixtures/response_mock/events.json'))
+        allow(Faraday).to receive(:new).and_return(connection_mock)
+        allow(connection_mock).to receive(:get).and_return(response_mock)
+      end
+
+      it 'returns events' do
+        events = ConnpassEvent::Client.new.events
+        expect(events.count).to eq(1)
+      end
+    end
+  end
+
+  describe '::Request' do
+    describe 'self.params' do
+      context 'when no params' do
+        it 'should be empty hash' do
+          expect(ConnpassEvent::Request.params).to eq({})
+        end
+      end
+
+      context 'when one defined param' do
+        it 'should be hash' do
+          expect(ConnpassEvent::Request.params(keyword: 'ruby')).to eq({ keyword: 'ruby' })
+        end
+      end
+
+      context 'when one undefined param' do
+        it 'should be empty hash' do
+          expect(ConnpassEvent::Request.params(undefined: 'ruby')).to eq({})
+        end
+      end
+    end
   end
 end
+# rubocop:enable Metrics/BlockLength
